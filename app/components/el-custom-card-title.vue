896ac0c9a9a25b398a861a959b45a64a0eaf070d
sunjie <sunjiezhu@foxmail.com>
2026-06-21 21:50:36 +0800

    feat: 新增el-card自定义标题组件

---
+++ app/components/el-custom-card-title.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps({
+    barColor: { type: String, default: "#409eff" },
+    barWidth: { type: String, default: "4px" }
+})
+</script>
+
+<template>
+    <el-row align="middle" class="mb-[18px]">
+        <div class="title-bar"></div>
+
+        <span class="font-bold">
+            <slot name="default"></slot>
+        </span>
+    </el-row>
+</template>
+
+<style>
+.title-bar {
+    width: v-bind(barWidth);
+    height: 16px;
+    background-color: v-bind(barColor);
+    margin-right: 10px;
+    border-radius: 2px;
+}
+</style>
